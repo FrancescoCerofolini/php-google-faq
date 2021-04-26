b852f7d2bfd8030da3e8947b8c97961a8420fbdf
FrancescoCerofolini <f.cerofolini96@gmail.com>
2021-04-26 16:20:40 +0200

    Situazione inziale

---
+++ src/index.php
@@ -0,0 +1,26 @@
+<?php
+    $menunav = ["Introduzione", "Norme sulla privacy", "Termini di servizio", "Tecnologie", "Domande frequenti"];
+    $faq = [
+        [
+            "question" => "",
+            "answer" => "",
+        ],
+        [
+            "question" => "",
+            "answer" => "",
+        ],
+        [
+            "question" => "",
+            "answer" => "",
+        ],
+        [
+            "question" => "",
+            "answer" => "",
+        ],
+        [
+            "question" => "",
+            "answer" => "",
+        ],
+
+    ];
+?>
